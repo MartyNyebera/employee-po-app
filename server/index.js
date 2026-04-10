422a8fd50b508c1d823276bccdf060a4c877777f
SLASHBUTCODING <22-01684@g.batstate-u.edu.ph>
2026-04-10 12:30:57 +0800

    feat: enable real-time GPS tracking with SSE and Leaflet map

--- server/index.js
+++ server/index.js
@@ -59,10 +59,10 @@ app.use(express.urlencoded({ extended: true }));
 
 // Add CSP headers to allow frontend connections
 app.use((req, res, next) => {
-  res.setHeader('Content-Security-Policy', 
+  res.setHeader('Content-Security-Policy',
     "default-src 'self'; " +
-    "script-src 'self' 'unsafe-inline' 'unsafe-eval'; " +
-    "style-src 'self' 'unsafe-inline' https://fonts.googleapis.com; " +
+    "script-src 'self' 'unsafe-inline' 'unsafe-eval' https://unpkg.com; " +
+    "style-src 'self' 'unsafe-inline' https://fonts.googleapis.com https://unpkg.com; " +
     "img-src 'self' data: blob:; " +
     "connect-src 'self' ws: wss: http://localhost:3000 https://localhost:3000; " +
     "font-src 'self' https://fonts.gstatic.com; " +
